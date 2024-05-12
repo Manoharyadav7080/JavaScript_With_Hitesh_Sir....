e2c45122a74c5319d221ccf524b80cf733412f8b
Manohar Yadav <113102874+Manoharyadav7080@users.noreply.github.com>
2024-05-12 21:28:04 +0000

    Variable in js

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,23 @@
+const accountId = 21347;
+let accountEmail = "yadavhima78094@gmail.com";
+var accountPassword = "5121369";
+accountCity = "jaipur";  // this are possible but not good idea.
+let accountState; 
+
+
+
+// accountId =  23;  // not allowed 
+// Var variable are not flow block Scope  and functional Scope ("{ }"). if change variable value the reflect it every Scope and file ;
+/*
+Prefer not to use Var 
+because of issue in block Scope and functional Scope 
+*/
+
+
+
+accountEmail="yadavmanohar78094@gmail.com"
+accountPassword= "2147891278";
+accountCity ="japan";
+
+console.log(accountEmail);
+console.table([accountEmail, accountId,accountPassword, accountCity , accountState]);
