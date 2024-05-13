04211453b628b5657a36a46c78becd94ab28d009
Manohar Yadav <113102874+Manoharyadav7080@users.noreply.github.com>
2024-05-13 22:22:33 +0000

    composition  in js

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,76 @@
+//console.log(2>1); // true 
+//console.log(2>=1); // true 
+//console.log(2<1); // false 
+//console.log(2 == 1); // false 
+//console.log(2 != 1); // true 
+
+
+
+console.log("2" > 1); // true 
+console.log("2" == 2); // true
+
+console.log("02" > 1); // true 
+
+
+
+// in this side reflect  are confusion because in the js comparision (< , > ,>= , >=  , <+   etc ) and equity(=)  are flow deffrent type conversion  
+console.log(null > 0); // false  because 0>0 (false )
+console.log(null == 0); // in this side  adding a conversion methods by the js ; therefor  null ---> (0)== 0  // false 
+console.log(null >= 0);  // 0>= 0 (true)
+
+
+//    ===  -->   this  are used for chack  avriable equality with datatype ;
+console.log("2" === 2); // false 
+
+
+
+// undefined 
+console.log(undefined == 0); // false 
+console.log(undefined > 0);// false 
+console.log(undefined <  0); //  false 
+
+
+
+// Note :-   In the js , comparision and equality  dono aalg  aalag  hai ;
+// read by the ECMA Script Documentation;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
