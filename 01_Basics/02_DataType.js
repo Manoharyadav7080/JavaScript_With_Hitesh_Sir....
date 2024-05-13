d9fe3cb2b677f6c44e91fbd0a64d9a85846d2f94
Manohar Yadav <113102874+Manoharyadav7080@users.noreply.github.com>
2024-05-13 19:54:26 +0000

    DataTypes

---
+++ 01_Basics/02_DataType.js
@@ -0,0 +1,119 @@
+"use strict "; // treat all js as newer version.
+
+//  alert(3+3) ;  // we are useing Node.js not a browser   -------> javaScript ka Engine browser ke andar hota hai ; 
+
+console.log( 3+ 
+    3
+)   // this is allow in JS but not flow Readability;
+// code readability  should be high ;
+
+// Documentation :-   Tc39.es (ecma-Standerd)   AND MDN 
+
+console.log("hii Sir🎯")
+
+// DATAType 
+let name = "Manohar Yadav" ;
+let age = 22 ;
+let isLoggedI = false ;
+  /* 
+                    Permeative DataType 
+   1.  number => 2 to power 53 
+   2.  bigint                   (Used by only -> Treding ,StockMarket , Redit , Facebook , Google , microsoft);
+   3.  string => "";
+   4 . boolean => true / false 
+   5 . null  => standlone value 
+   6 . undefined 
+   7. symbole => 
+
+
+    // Object
+
+  
+  */
+
+console.log(typeof name);
+console.log(typeof age );
+
+//  But 
+
+console.log( typeof null); //   null is a  object
+//AND 
+console.log(typeof undefined); // undef
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
