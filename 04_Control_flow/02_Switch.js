6c307fc128d32a6375ed934a57a23a323af81bcd
Manohar Yadav <113102874+Manoharyadav7080@users.noreply.github.com>
2024-05-19 07:28:24 +0000

    switch Case

---
+++ 04_Control_flow/02_Switch.js
@@ -0,0 +1,63 @@
+//Basic Syntax 
+
+/*
+
+    switch(key) {
+        case value:
+            break;
+
+        default:
+             break;
+    }
+
+*/
+
+
+// ___________________________________________________________________________________________________________________________
+
+
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("january");    
+        break;
+    case 2:
+        console.log("Febuary");    
+        break;
+    case 3:
+        console.log("march");    
+        break;
+    case 4:
+        console.log("April");    
+        break;
+        
+    default:
+        console.log("Default Case march ");    
+        con
+        break;
+}
+// Answer :-   march
+
+//  Other 
+
+const month1 = "April";
+switch (month1) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match ")
+        break;
+}
+// Answer :- April
+
